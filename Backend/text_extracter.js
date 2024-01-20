ae47152584a45ffa84c6e14ed1fda1551dc3a54f
segfal <segfaultboi@gmail.com>
2024-01-20 14:46:53 -0500

    craeted the text_extracter function

--- Backend/text_extracter.js
+++ Backend/text_extracter.js
@@ -1,3 +1,21 @@
 const pdf = require('pdf-parse');
 const fs = require('fs');
 
+
+
+const pdfPath = 'transcript.pdf'
+
+const textExtracter = (filename) => {
+    
+    let dataBuffer = fs.readFileSync(filename);
+    pdf(dataBuffer).then(function(data) {
+      console.log(data.text);
+    }).catch(error => {
+      console.log("Error extracting text: " + error.message);
+    });
+    
+
+
+}
+
+let x = textExtracter(pdfPath)
