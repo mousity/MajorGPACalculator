0691199c66e37e38e13797bf6fb126ddf8dd3d8c
darausz <darauszhang@gmail.com>
2024-01-21 01:06:44 -0500

    set up website layout

--- MajorGPACalculator/src/App.jsx
+++ MajorGPACalculator/src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/logo.png'
 import axios from 'axios'
 import './App.css'
 
@@ -26,7 +25,7 @@ function App() {
       //   },
       //   body: JSON.stringify({ imageUrl }),
       // });
-  
+
       // if (!response.ok) {
       //   throw new Error('OCR failed');
       // }
@@ -38,30 +37,49 @@ function App() {
       setError(err.message);
     }
   };
-  
-  
+
+
   useEffect(() => {
-    
+
   }, [])
   return (
     <>
       <div className='heading'>
-        
+        <img className="logo" src={logo}></img>
+        <h1>Major GPA Calculator</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <input type="file" onChange={handleFile} />
-        <button onClick={callOcrApi}>
-          Upload
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+
+      <div className='card'>
+        <p className='desc'>
+          Welcome to our page! Feel free to upload your student transcript in pdf format for a quick calculation of your major GPA.
         </p>
+        <form className='form'>
+          <input type='file' onChange={handleFile} />
+          <button onClick={callOcrApi}>
+            Upload
+          </button>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className='majorGPA'>
+        Major GPA:
       </p>
-       {result ? result:null}
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Course</th>
+            <th>Credits</th>
+            <th>GPA</th>
+          </tr>
+        </thead>
+        <tbody> {/*dummy data*/}
+          <tr> <td>CSCI 101</td> <td>3.00</td> <td>4</td></tr>
+          <tr> <td>CSCI 102</td> <td>3.00</td> <td>3.7</td></tr>
+          <tr> <td>CSCI 103</td> <td>3.00</td> <td>2</td></tr>
+        </tbody>
+      </table>
+
+
+      {result ? result : null}
     </>
   )
 }
